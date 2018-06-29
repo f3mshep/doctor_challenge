22f0c74f4860d5af85dc4d4594fc634f4574b6e2
f3mshep <superbiscuit@gmail.com>
2018-06-29 12:12:34 -0700

    added average_rating calculation for doctor

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,17 @@
 class Doctor < ApplicationRecord
   has_many :ratings
   has_many :specialties
+
+  attr_accessor :average_rating
+
+  def calculate_rating
+    total = 0
+    self.ratings.each do |rating|
+      total += rating.score
+    end
+    total = total / self.ratings.size
+    average_rating = total
+    total
+  end
+
 end
