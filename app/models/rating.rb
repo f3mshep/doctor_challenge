5a1cf42615d96d876ad52ad1c84879a75f38d008
Alexandra <github email address>
2018-06-29 21:07:56 -0700

    added validation

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,9 @@
 class Rating < ApplicationRecord
   belongs_to :doctor
   after_save :calculate_rating
+  validates :score, numericality: true
+  validates :score, numericality: { less_than_or_equal_to: 5 }
+  validates :score, numericality: { greater_than_or_equal_to: 1 }
 
   def calculate_rating
     # triggers doctor updating average_rating by saving doctor
